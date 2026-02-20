ae495f807c2e65c88ddca015e2ae16506b754492
joshliford <j_liford1@yahoo.com>
2026-02-19 21:32:23 -0600

    added validation dependency; created User model and User repository

---
+++ src/main/java/com/github/joshliford/amplifyguitar/model/User.java
@@ -0,0 +1,179 @@
+package com.github.joshliford.amplifyguitar.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String passwordHash;
+
+    @Column(unique = true)
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 40, message = "First name must be between 2 and 40 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 40, message = "Last name must be between 2 and 40 characters")
+    private String lastName;
+
+    private String displayName;
+
+    @NotBlank
+    private Integer currentLevel;
+
+    @NotBlank
+    private Integer totalXp;
+
+    @NotBlank
+    private Integer currentXp;
+
+    @NotBlank
+    private Integer currentStreak;
+
+    @NotBlank
+    private Integer longestStreak;
+
+    private LocalDateTime lastPracticeDate;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime createdAt;
+
+    public User(Integer currentLevel, Integer currentStreak, Integer currentXp, String displayName, String email, String firstName, String lastName, LocalDateTime lastPracticeDate, Integer longestStreak, String passwordHash, Integer totalXp) {
+        this.currentLevel = currentLevel;
+        this.currentStreak = currentStreak;
+        this.currentXp = currentXp;
+        this.displayName = displayName;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.lastPracticeDate = lastPracticeDate;
+        this.longestStreak = longestStreak;
+        this.passwordHash = passwordHash;
+        this.totalXp = totalXp;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getCurrentLevel() {
+        return currentLevel;
+    }
+
+    public void setCurrentLevel(Integer currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public Integer getCurrentStreak() {
+        return currentStreak;
+    }
+
+    public void setCurrentStreak(Integer currentStreak) {
+        this.currentStreak = currentStreak;
+    }
+
+    public Integer getCurrentXp() {
+        return currentXp;
+    }
+
+    public void setCurrentXp(Integer currentXp) {
+        this.currentXp = currentXp;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDateTime getLastPracticeDate() {
+        return lastPracticeDate;
+    }
+
+    public void setLastPracticeDate(LocalDateTime lastPracticeDate) {
+        this.lastPracticeDate = lastPracticeDate;
+    }
+
+    public Integer getLongestStreak() {
+        return longestStreak;
+    }
+
+    public void setLongestStreak(Integer longestStreak) {
+        this.longestStreak = longestStreak;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Integer getTotalXp() {
+        return totalXp;
+    }
+
+    public void setTotalXp(Integer totalXp) {
+        this.totalXp = totalXp;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
