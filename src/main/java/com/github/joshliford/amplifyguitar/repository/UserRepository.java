db697dcea7596cd281bca351bc4febf2f0bcbf14
joshliford <j_liford1@yahoo.com>
2026-02-21 13:59:34 -0600

    implement CustomUserDetailsService for user authentication; add UserRepository method to find user by email

--- src/main/java/com/github/joshliford/amplifyguitar/repository/UserRepository.java
+++ src/main/java/com/github/joshliford/amplifyguitar/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.github.joshliford.amplifyguitar.repository;
 import com.github.joshliford.amplifyguitar.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/github/joshliford/amplifyguitar/security/CustomUserDetailsService.java
+++ src/main/java/com/github/joshliford/amplifyguitar/security/CustomUserDetailsService.java
@@ -1,4 +1,38 @@
 package com.github.joshliford.amplifyguitar.security;
 
-public class CustomUserDetailsService {
+import com.github.joshliford.amplifyguitar.model.User;
+import com.github.joshliford.amplifyguitar.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // override Spring default UserDetails with custom User object (using email as username)
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User existingUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(existingUser.getEmail()) // override and set email as username
+                .password(existingUser.getPasswordHash())
+                .authorities(Collections.emptyList()) // Amplify does not use RBAC so leave as empty list
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+
 }
