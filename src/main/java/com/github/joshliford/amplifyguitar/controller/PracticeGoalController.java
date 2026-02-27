3cac84bfde4027af19574b2a298e25a15df14dac
joshliford <j_liford1@yahoo.com>
2026-02-26 22:15:54 -0600

    add PracticeGoalController and PracticeGoalService; implement GET endpoint to retrieve all practice goals

---
+++ src/main/java/com/github/joshliford/amplifyguitar/controller/PracticeGoalController.java
@@ -0,0 +1,35 @@
+package com.github.joshliford.amplifyguitar.controller;
+
+import com.github.joshliford.amplifyguitar.model.PracticeGoal;
+import com.github.joshliford.amplifyguitar.service.PracticeGoalService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/*
+1 endpoint:
+GET /api/goals (get all practice goals)
+*/
+
+@RestController
+@RequestMapping("/api/goals")
+public class PracticeGoalController {
+
+    private final PracticeGoalService practiceGoalService;
+
+    public PracticeGoalController(PracticeGoalService practiceGoalService) {
+        this.practiceGoalService = practiceGoalService;
+    }
+
+    @GetMapping("")
+    // ResponseEntity wraps the list to have control over HTTP status
+    public ResponseEntity<List<PracticeGoal>> getAllGoals() {
+        List<PracticeGoal> response = practiceGoalService.getAllGoals();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+}
