c7272519c3997869c6f0408b7f49fcaffe858565
joshliford <j_liford1@yahoo.com>
2026-02-28 10:43:29 -0600

    add CORS configuration to SecurityConfig; enable CORS support with allowed origins, methods, and headers

--- src/main/java/com/github/joshliford/amplifyguitar/config/SecurityConfig.java
+++ src/main/java/com/github/joshliford/amplifyguitar/config/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable()) // disable CSRF (Cross-Site Request Forgery)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT-based authentication (doesn't use session cookies)
@@ -52,6 +58,21 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // set allowed origins, methods, and headers
+        configuration.setAllowedOrigins((List.of("http://localhost:5173")));
+        configuration.setAllowedMethods((List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS")));
+        configuration.setAllowedHeaders((List.of("*Authorization", "Content-Type")));
+        // required to allow Auth header to be sent
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // apply config globally
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
