6f7f97fab79f33e5246565b813c6bf41da9b339c
joshliford <j_liford1@yahoo.com>
2026-03-02 19:08:19 -0600

    add chordService and scaleService for fetching chord/scale data from the API

---
+++ react-front-end-app/src/services/scaleService.js
@@ -0,0 +1,15 @@
+import apiClient from "@/config/axiosConfig";
+
+// handles fetching scale data from the API
+
+export function getAllScales() {
+    return apiClient.get('/scales');
+}
+
+export function getAllScalesByDifficulty(difficulty) {
+    return apiClient.get(`/scales/difficulty/${difficulty}`);
+}
+
+export function getScaleById(id) {
+    return apiClient.get(`/scales/${id}`);
+}
