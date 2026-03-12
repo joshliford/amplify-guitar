a01eb5f280b2273578f46f8be0fe4b3b76adf41a
joshliford <j_liford1@yahoo.com>
2026-03-11 22:16:36 -0500

    add OneToMany relationship to practice sessions and user lessons in User model for deletion operations; add delete user endpoint in UserController

--- java-spring-boot-back-end-app/src/main/java/com/github/joshliford/amplifyguitar/controller/UserController.java
+++ java-spring-boot-back-end-app/src/main/java/com/github/joshliford/amplifyguitar/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,14 @@ public class UserController {
                 currentUser.getTotalXp()
         );
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal UserDetails userDetails) {
+        String email = userDetails.getUsername();
+        User currentUser = userService.findByEmail(email);
+        userService.deleteUser(currentUser.getId());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
